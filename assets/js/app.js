3e7c7ea7b3507f376e719e849c48d587dc34476e
Thales Monteiro <the.monteiro.dev@gmail.com>
2016-09-29 03:54:09 -0300

    let's see what happens

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,7 +34,6 @@ $(document).ready(function () {
         $(this).addClass('active');
 
         var target = this.hash,
-            menu = target;
         $target = $(target);
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top - $('.header-bar').height()
